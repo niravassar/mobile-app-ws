780624b0bdeb423b4dee96ed6ff9ac7a1c2bc70f
nassar <assarn@objectcomputing.com>
2023-06-28 12:53:56 -0500

    return user

--- src/main/java/com/appsdeveloperblog/apps/ws/mobileappws/UserController.java
+++ src/main/java/com/appsdeveloperblog/apps/ws/mobileappws/UserController.java
@@ -1,14 +1,26 @@
 package com.appsdeveloperblog.apps.ws.mobileappws;
 
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
+    @GetMapping
+    public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
+                           @RequestParam(value="limit", defaultValue= "50") int limit,
+                           @RequestParam(value="sort", required = false) String sort) {
+        return "getUser was called page = " + page + " and limit = " + limit + " and sort = " + sort;
+    }
+
     @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable String userId) {
-        return "getUser was called userId = " + userId;
+    public UserRest getUser(@PathVariable String userId) {
+        UserRest userRest = new UserRest();
+        userRest.setEmail("test@test.com");
+        userRest.setFirstName("Nirav");
+        userRest.setLastName("Assar");
+        return userRest;
     }
 
     @PostMapping
